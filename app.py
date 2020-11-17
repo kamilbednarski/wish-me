a6ef45f0f8ed4a10475ca9adc229696e778cb801
Kamil Bednarski <kamilarturbednarski@gmail.com>
2020-11-17 14:13:31 +0100

    new: change password

--- app.py
+++ app.py
@@ -35,38 +35,11 @@ db = SQL("sqlite:///wish.db")
 @app.route("/register", methods=["GET", "POST"])
 def register():
     # This method registers new users
-
     # Forget any user id
     session.clear()
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-
-        # Ensure username was submitted
-        # TODO:
-        # username at least 3 letter long
-        #if not request.form.get("username"):
-        #    return render_template("register.html")
-        
-
-        # Ensure password was submitted
-        # TODO:
-        # compare password and confirmation (javascript potentially)
-        # password at least 8 char long
-        #if not request.form.get("password"):
-        #   return render_template("register.html")
-
-        
-        # Query database for username
-        #rows = db.execute("SELECT * FROM users WHERE username = :username",
-                          #username=request.form.get("username"))
-        
-        # Ensure that username isn't already in database
-        # TODO:
-        # Redirect to register page with message that username is already in use
-        #if len(rows) != 0:
-        #   return render_template("register.html")
-
 
         # Save user data submitted in form
         name = str(request.form.get("first-name"))
@@ -83,7 +56,8 @@ def register():
         db.execute("INSERT INTO users (username, hash, name, surname, email, city, country) VALUES (:iusername, :ihash, :iname, :isurname, :iemail, :icity, :icountry)", 
                     iusername=username, ihash=hash, iname=name, isurname=surname, iemail=email, icity=city, icountry=country)
 
-        return render_template("login.html")
+        # Redirect to login panel
+        return redirect("/login")
 
     else:
         return render_template("register.html")
@@ -92,10 +66,8 @@ def register():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     # This method is used to log in
-
     # Forget any user id
     session.clear()
-    print("START")
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
@@ -134,28 +106,69 @@ def login():
 @app.route("/logout")
 @login_required
 def logout():
+    # This method is used to log out
     session.clear()
+    # Redirect to log in panel
     return redirect("/login")
 
 @app.route("/")
 @login_required
 def index():
+    # TODO
     return render_template("index.html")
 
 @app.route("/profile")
 @login_required
 def user():
+    # This method redirects users profile with dynamic url
     return redirect(f"/profile/{session['username']}")
 
 
 @app.route("/profile/<username>")
 @login_required
 def user_profile(username):
+    # This method displays user's account card
+    # Query database for account details where userids are equal
     user_data = db.execute("SELECT * FROM users WHERE id = :id", id=session["user_id"])
+
+    # Save data
     name = user_data[0]["name"]
     surname = user_data[0]["surname"]
     email = user_data[0]["email"]
     city = user_data[0]["city"]
     country = user_data[0]["country"]
     
+    # Render user's account page
     return render_template("profile.html", username=username, name=name, surname=surname, email=email, city=city, country=country)
+
+@app.route("/change/password", methods=["GET", "POST"])
+@login_required
+def change_password():
+    # This method changes password
+    if request.method == "POST":
+
+        # Old password and new password input fields are both set to required in html code
+        # Additional check if password was submited
+        if not request.form.get("password") or not request.form.get("password-new"):
+            print("INFO: additional password submit check failed; route: change/password")
+            return redirect("/change/password")
+
+        if request.form.get("password") != request.form.get("password-new"):
+            print("INFO: additional check failed: password and new password not equal; route: change/password")
+            return redirect("/change/password")
+
+        # Query database for account details
+        rows = db.execute("SELECT id, username, hash FROM users WHERE id = :id", id=session['user_id'])
+
+        # If there is no user with that id or password do not match, redirect
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return redirect("/change/password")
+        
+        # Generate hash for new password
+        new_hash = generate_password_hash(request.form.get("password-new"), method='pbkdf2:sha256', salt_length=8)
+
+        # Update existing hash with new hash
+        db.execute("UPDATE users SET hash = :new_hash WHERE id = :id", new_hash=new_hash, id=session['user_id'])
+        
+    else:
+        return render_template("changepassword.html")
